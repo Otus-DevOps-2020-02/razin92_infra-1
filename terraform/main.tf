8d90455a703155f563204c02d257b6064fd220d9
Vyacheslav Razin <s.razin92@gmail.com>
2019-09-29 15:39:59 +0500

    Completed tf-project for main part of the HW

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,21 +5,19 @@ terraform {
 
 provider "google" {
   # Версия провайдера
-  version = "2.15"
-
-  # ID проекта
-  project = "wise-cycling-253214"
-
-  region = "europe-west-1"
+  version = "2.15.0"
+  project = var.project
+  region = var.region
 }
 
 resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-b"
+  tags = ["reddit-app"]
   boot_disk {
     initialize_params {
-      image = "reddit-base"
+      image = var.disk_image
     }
   }
 
@@ -30,6 +28,38 @@ resource "google_compute_instance" "app" {
 
   metadata = {
     # Путь до публичного ключа
-    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")})"
+    ssh-keys = "appuser:${file(var.public_key_path)})"
   }
+
+  connection {
+    type = "ssh"
+    host = self.network_interface[0].access_config[0].nat_ip
+    user = "appuser"
+    agent = false
+    private_key = file("~/.ssh/appuser")
+  }
+
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Сеть, в которй действует правило
+  network = "default"
+  # Список разрешений
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  # Каким адресам рарешается доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
 }
